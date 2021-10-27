74f36b65ae7c5db6408b7550f23bda95858bbaaf
alecfrey <79166968+alecfrey@users.noreply.github.com>
2021-10-26 22:33:41 -0500

    Test

--- edu.iastate.cs228.hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
+++ edu.iastate.cs228.hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
@@ -1,12 +1,11 @@
 package edu.iastate.cs228.hw3;
 
+// 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
 public class Infix2Postfix {
 	
 	public static void main(String[] args) {
 		
-		
-		System.out.println("test2");
-		
+				
 		
 		
 		
